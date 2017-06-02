4df1717696752480e0174ad4f09c81892f3656de
Vitalii Rybalko <vrybalko@e1r8p4.unit.ua>
2017-06-02 20:20:10 +0300

    fasfdsd

--- sockets/my_sign.py
+++ sockets/my_sign.py
@@ -3,10 +3,16 @@
 import my_rsa
 import hashlib
 import json
+import base64
 
 def sign_data(data, keys_path):
     sha256_sum = hashlib.sha256(data).hexdigest()
-    sign = my_rsa.encode_withot_pad(sha256_sum, keys_path)
+    sign = base64.b64encode(str(my_rsa.encode_sign(sha256_sum, keys_path)), 'ascii')
     return (json.dumps((sign, data)))
 
-print(sign_data(b"asdfasdf", "../numbers.txt"))
+def verify_sign(signature, data, keys_path):
+    sha256_sum = hashlib.sha256(data).hexdigest()
+    decr_sign = my_rsa.decode_sign(int(base64.b64decode(signature, 'asscii')), keys_path)
+    print("sum =\n", sha256_sum)
+    print("decr_sign =\n", decr_sign)
+    return (sha256_sum == decr_sign)
