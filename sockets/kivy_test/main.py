7245f81d3765b1b6b5d28aefed80b0cf12aa0369
vilkoz <vilko.a@gmail.com>
2017-06-03 22:58:10 +0300

    ad fields to kyvi app

--- sockets/kivy_test/main.py
+++ sockets/kivy_test/main.py
@@ -16,14 +16,21 @@ class LoginScreen(GridLayout):
         self.add_widget(Label(text='message'))
         self.message = TextInput(multiline=False)
         self.add_widget(self.message)
+        self.add_widget(Label(text='server ip'))
+        self.ipaddr = TextInput(multiline=False)
+        self.add_widget(self.ipaddr)
+        self.add_widget(Label(text='path to keys'))
+        self.keys_path = TextInput(multiline=False)
+        self.add_widget(self.keys_path)
         self.send = Button(text="send")
         self.send.bind(on_press=self.send_data)
         self.add_widget(self.send)
 
     def send_data(self, obj):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("192.168.0.247", 50012))
-        encoded_int = (my_rsa.encode(self.message.text, "../../numbers.txt"));
+        s.connect((self.ipaddr.text, 50012))
+        encoded_int = (my_rsa.encode(self.message.text,
+            self.keys_path.text));
         encoded_bytes = my_rsa.pack(encoded_int)
         send_data1 = encoded_bytes 
         send_data1 += (b'\x00')
