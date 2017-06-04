be53eaf412f4bb236a24bc73b8f998d75835c621
vilkoz <vilko.a@gmail.com>
2017-06-05 02:00:55 +0300

    add latest selection save

--- sockets/kivy_test/main.py
+++ sockets/kivy_test/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 import socket
 import my_rsa
+import json
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
@@ -50,6 +51,10 @@ class LoginScreen(GridLayout):
         self.send = Button(text="send")
         self.send.bind(on_press=self.send_data)
         self.add_widget(self.send)
+        self.add_widget(Label(text="Output"))
+        self.output = Label(text="")
+        self.add_widget(self.output)
+        self.load_strings()
 
     def show_load(self, obj):
         content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
@@ -65,7 +70,38 @@ class LoginScreen(GridLayout):
         self.keys_path.text = path
         self.dismiss_popup()
 
+    def save_strings(self, homedir):
+        if not os.path.isdir(os.path.join(homedir, ".diplom")):
+            try:
+                os.mkdir(os.path.join(homedir, ".diplom"))
+            except:
+                print ("error creating folder" +
+                        os.path.join(homedir, ".diplom"))
+                pass
+        with open(os.path.join(homedir, ".diplom", "fields.json"), "w+") as f:
+            f.write(json.dumps({
+                "ip" : self.ipaddr.text,
+                "keys_path" : self.keys_path.text
+                }))
+
+    def load_strings(self):
+        try:
+            homedir = os.environ['HOME']
+        except:
+            homedir = "/storage/sdcard0/"
+        if os.path.exists(os.path.join(homedir, ".diplom", "fields.json")):
+            f = open(os.path.join(homedir, ".diplom", "fields.json"), "r")
+            contents = json.loads(f.read())
+            self.ipaddr.text = contents['ip'].encode()
+            self.keys_path.text = contents['keys_path'].encode()
+
     def send_data(self, obj):
+        try:
+            self.output.text = (os.environ['HOME'])
+            homedir = self.output.text
+        except:
+            self.output.text = "Android detected"
+            homedir = "/storage/sdcard0/"
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((self.ipaddr.text, 50012))
         encoded_int = (my_rsa.encode(self.message.text,
@@ -78,6 +114,7 @@ class LoginScreen(GridLayout):
         # data = s.recv(1024)
         s.close()
         # print ('Received ', (data.decode())) 
+        self.save_strings(homedir)
 
 class MyApp(App):
 
