0a0d3ef26375d2ccb7ee4887667e5f939e6611bd
vilkoz <vilko.a@gmail.com>
2017-06-05 14:55:53 +0300

    pedantic

--- sockets/my_sign.py
+++ sockets/my_sign.py
@@ -7,17 +7,19 @@ import base64
 
 def sign_data(data, keys_path):
     sha256_sum = hashlib.sha256(data).hexdigest()
-    sign = base64.b64encode(str(my_rsa.encode_sign(sha256_sum, keys_path)), 'ascii')
+    sign = base64.b64encode(str(my_rsa.encode_sign(sha256_sum, keys_path)),
+            'ascii')
     return sign
 
 def verify_sign(signature, data, keys_path):
     sha256_sum = hashlib.sha256(data).hexdigest()
-    decr_sign = my_rsa.decode_sign(int(base64.b64decode(signature, 'asscii')), keys_path)
+    decr_sign = my_rsa.decode_sign(int(base64.b64decode(signature, 'asscii')),
+            keys_path)
     print("sum =\n", sha256_sum)
     print("decr_sign =\n", decr_sign)
     return (sha256_sum == decr_sign)
 
 if __name__ == "__main__":
-    data = "my data"
-    sign = sign_data(data, "../numbers.txt")
-    print(verify_sign(sign, data, "../numbers.txt"))
+    data_ = "my data"
+    sign_ = sign_data(data_, "../numbers.txt")
+    print(verify_sign(sign_, data_, "../numbers.txt"))
